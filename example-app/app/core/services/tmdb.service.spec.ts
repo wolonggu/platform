1af366b59de54a50fb66371416fe67cadab1003c
shanhong cheng <shnahongcheng@gmail.com>
2018-01-22 21:38:06 -0700

    fix tmdn service spec

--- example-app/app/core/services/tmdb.service.spec.ts
+++ example-app/app/core/services/tmdb.service.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
 
 import { TmdbService } from './tmdb.service';
-
+import { TmdbConfigService } from '../../movies/services/tmdb-config.service';
 describe('TmdbService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TmdbService],
+      providers: [
+        TmdbService,
+        TmdbConfigService,
+        { provide: HttpClient, useValue: { get: jest.fn() } },
+      ],
     });
   });
 

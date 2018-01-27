ad1d12b29617e68a108eb794563d8765a9fd4176
Shanhong Cheng <scheng@InComm.com>
2018-01-27 10:46:16 -0700

    bootstrap

--- example-app/app/core/containers/app.ts
+++ example-app/app/core/containers/app.ts
@@ -11,33 +11,39 @@ import * as Auth from '../../auth/actions/auth';
   selector: 'bc-app',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <bc-layout>
-      <bc-sidenav [open]="showSidenav$ | async">
-        <bc-nav-item (navigate)="closeSidenav()" *ngIf="loggedIn$ | async" routerLink="/admin" icon="security" hint="Admin Dashboard">
-          Admin Dashboard
-        </bc-nav-item>
-        <bc-nav-item (navigate)="closeSidenav()" *ngIf="loggedIn$ | async" routerLink="/movies" icon="security" hint="Movie list">
-          Movies
-        </bc-nav-item>
-        <bc-nav-item (navigate)="closeSidenav()" *ngIf="loggedIn$ | async" routerLink="/" icon="book" hint="View your book collection">
-          My Collection
-        </bc-nav-item>
-        <bc-nav-item (navigate)="closeSidenav()" *ngIf="loggedIn$ | async" routerLink="/books/find" icon="search" hint="Find your next book!">
-          Browse Books
-        </bc-nav-item>
-        <bc-nav-item (navigate)="closeSidenav()" *ngIf="!(loggedIn$ | async)">
-          Sign In
-        </bc-nav-item>
-        <bc-nav-item (navigate)="logout()" *ngIf="loggedIn$ | async">
-          Sign Out
-        </bc-nav-item>
-      </bc-sidenav>
-      <bc-toolbar (openMenu)="openSidenav()">
-        Book Collection
-      </bc-toolbar>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Navbar</a>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" *ngIf="loggedIn$ | async" routerLink="/admin">Admin Dashboard <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" *ngIf="loggedIn$ | async" routerLink="/movies">Movies</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" *ngIf="loggedIn$ | async" routerLink="/">Browse books</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" *ngIf="loggedIn$ | async" routerLink="/books/find">Find your books</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" *ngIf="loggedIn$ | async">Sign out</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
 
       <router-outlet></router-outlet>
-    </bc-layout>
   `,
 })
 export class AppComponent {
